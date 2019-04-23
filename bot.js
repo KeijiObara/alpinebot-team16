acb04db99ddcb8625b451f85b75014e50dbbd50c
FUKAWA Shinnosuke 府川 慎之介 <fukawas@daikin.co.jp>
2019-04-23 13:24:08 +0900

    v3 add button&answer

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityTypes } = require('botbuilder');
+//const { ActivityTypes } = require('botbuilder');
+const { ActivityTypes, MessageFactory, TurnContext } = require('botbuilder');
+const button_list = ['FAQs', 'Band Search', 'Navigate']
 
 class MyBot {
     /**
@@ -16,17 +18,22 @@ class MyBot {
     async onTurn(turnContext) {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         if (turnContext.activity.type === ActivityTypes.Message) {
-            await turnContext.sendActivity(`You speak '${ turnContext.activity.text }'`);
-
+            if (button_list.includes(turnContext.activity.text)) {
+                await turnContext.sendActivity(`'${ turnContext.activity.text }'ボタンが押されました`);
+            }else{
+                await turnContext.sendActivity(`もう一度おしえてください`);  
+            }
         } else if(turnContext.activity.type === ActivityTypes.ConversationUpdate) {
                 if (this.memberJoined(turnContext.activity)) {
-                    await turnContext.sendActivity(`Hey there! Welcome to the food bank bot. I'm here to help orchestrate 
-                                                    the delivery of excess food to local food banks!`);
-                    //await dialogContext.beginDialog(MENU_DIALOG);
+                    await turnContext.sendActivity(`Alpine Ski House音楽フェスティバルへようこそ！
+                                                    イベントのガイドをします。知りたいことを教えてください。`);
+//                    await dialogContext.beginDialog(MENU_DIALOG);
                 }
         } else {
             await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
         }
+        var reply = MessageFactory.suggestedActions(button_list);
+        await turnContext.sendActivity(reply);
     }
 }
 
